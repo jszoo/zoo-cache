d941953b385da0dcbbf7fe4231b5482eee0bd1e6
ruleechen <ronglin@kooboo.com>
2014-08-05 21:42:38 +0800

    update

---
+++ test/all.js
@@ -0,0 +1,23 @@
+/*
+* all
+* author: ruleechen
+* contact: rulee@live.cn
+* create date: 2014.8.4
+*/
+
+'use strict';
+
+var cache = require('..');
+var assert = require('assert');
+
+describe('store', function() {
+    describe('.set', function() {
+
+    });
+});
+
+describe('cache', function() {
+    describe('.set', function() {
+
+    });
+});
