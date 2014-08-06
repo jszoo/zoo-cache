4a47aafdc225d2a23f83931e57cd8bafd66c8f16
Ronglin-Kooboo <ronglin@kooboo.com>
2014-08-06 10:13:33 +0800

    all tests

--- test/all.js
+++ test/all.js
@@ -9,15 +9,84 @@
 
 var cache = require('..');
 var assert = require('assert');
+var animals = cache.region('animals');
 
-describe('store', function() {
-    describe('.set', function() {
+describe('cache', function() {
+    
+    it('.set 2 items', function() {
+        assert.equal(animals.set('cat1', 'cat1'), undefined);
+        assert.equal(animals.set('cat2', 'cat2'), undefined);
+    });
 
+    it('.get one of the 2 item', function() {
+        assert.equal(animals.get('cat1'), 'cat1');
+    });
+
+    it('.set 1 item with expire', function() {
+        animals.set('cat3', 'cat3', new Date(), function(param) {
+            assert.equal(param.action, 'expire');
+            assert.equal(param.value, 'cat3');
+        });
+    });
+
+    it('.get the expired item', function() {
+        assert.equal(animals.get('cat3'), null);
+    });
+
+    it('.all', function() {
+        assert.equal(animals.all()['cat1'], 'cat1');
+    });
+
+    it('.count', function() {
+        assert.equal(animals.count(), 2);
+    });
+
+    it('.exists', function() {
+        assert.equal(animals.exists('cat1'), true);
+    });
+
+    it('.remove', function() {
+        assert.equal(animals.remove('cat1'), true);
+    });
+
+    it('.exists again after remove', function() {
+        assert.equal(animals.exists('cat1'), false);
+    });
+
+    it('.clear', function() {
+        assert.equal(animals.clear(), true);
+    });
+
+    it('.count again after clear', function() {
+        assert.equal(animals.count(), 0);
     });
 });
 
-describe('cache', function() {
-    describe('.set', function() {
+describe('store', function() {
+    var store = animals.sto();
+    
+    it('.set', function() {
+        assert.equal(store.set('test_region', 'key', 'value'), undefined);
+        assert.equal(store.set('test_region', 'key2', 'value2'), undefined);
+    });
 
+    it('.get', function() {
+       assert.equal(store.get('test_region', 'key'), 'value');
+    });
+
+    it('.exists', function() {
+        assert.equal(store.exists('test_region', 'key'), true);
+    });
+
+    it('.remove', function() {
+        assert.equal(store.remove('test_region', 'key'), true);
+    });
+
+    it('.exists again after remove', function() {
+        assert.equal(store.exists('test_region', 'key'), false);
+    });
+
+    it('.clear', function() {
+        assert.equal(store.clear('test_region'), true);
     });
 });
